23cd814dd2e889434993fc0d698704d96a8e713b
abidmmp <abidmmp99@gmail.com>
2018-09-22 10:03:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -214,3 +214,4 @@
 // Extract config constants - 2024-10-07 10:01:00
 // Add type annotations - 2020-04-19 10:11:00
 // Fix edge case in parser - 2023-07-03 10:07:00
+// Clean up unused variables - 2018-09-22 10:03:00
