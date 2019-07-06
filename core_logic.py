46dbaee4abcbd90a399e655079a88303f6415d91
abidmmp <abidmmp99@gmail.com>
2019-07-06 10:07:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -122,3 +122,4 @@
 // Add logging statements - 2024-08-18 10:05:00
 // Fix recursive base case - 2022-07-27 10:01:00
 // Refactor class hierarchy - 2020-08-01 10:11:00
+// Add missing return statement - 2019-07-06 10:07:00
