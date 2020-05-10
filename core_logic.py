4d9b4b845514eb73a315e9385d52a5d7e9769d3d
abidmmp <abidmmp99@gmail.com>
2020-05-10 10:03:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -256,3 +256,4 @@
 // Fix edge case in parser - 2023-04-22 10:01:00
 // Improve type hints - 2024-03-24 10:11:00
 // Add unit test coverage - 2024-06-17 10:07:00
+// Add validation logic - 2020-05-10 10:03:00
