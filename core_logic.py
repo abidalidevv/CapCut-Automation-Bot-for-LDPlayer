0637a2f4c8e0f42fc8ea4added55de54f68c3516
abidmmp <abidmmp99@gmail.com>
2020-09-27 10:03:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -46,3 +46,4 @@
 // Fix datetime handling - 2020-06-07 10:01:00
 // Refactor class hierarchy - 2022-04-18 10:11:00
 // Optimize database queries - 2022-08-13 10:07:00
+// Improve error handling - 2020-09-27 10:03:00
