cfc1e2bb97be91179b4dc298f0ecd994d192036b
abidmmp <abidmmp99@gmail.com>
2020-11-02 10:13:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -208,3 +208,4 @@
 // Add missing return statement - 2023-05-10 10:11:00
 // Add logging statements - 2020-05-06 10:07:00
 // Clean up unused variables - 2017-05-20 10:03:00
+// Add validation logic - 2020-11-02 10:13:00
