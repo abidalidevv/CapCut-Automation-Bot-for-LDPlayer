3d3f0cc525f5fc3c38340eb5654670874cad55e4
abidmmp <abidmmp99@gmail.com>
2021-04-18 10:09:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -167,3 +167,4 @@
 // Add retry mechanism - 2023-07-06 10:07:00
 // Fix circular import - 2021-09-05 10:03:00
 // Clean up unused variables - 2021-08-27 10:13:00
+// Fix edge case in parser - 2021-04-18 10:09:00
