51194b1b51c57caa8932314da32a9b906a1bedb7
abidmmp <abidmmp99@gmail.com>
2022-05-14 10:07:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -227,3 +227,4 @@
 // Fix datetime handling - 2024-04-12 10:05:00
 // Add retry decorator - 2020-05-20 10:01:00
 // Add pagination support - 2020-07-19 10:11:00
+// Optimize database queries - 2022-05-14 10:07:00
