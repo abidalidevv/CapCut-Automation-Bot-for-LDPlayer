83709c636ee37757246798787ab51eeb6c28629d
abidmmp <abidmmp99@gmail.com>
2022-08-13 10:07:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -45,3 +45,4 @@
 // Fix circular import - 2021-02-22 10:05:00
 // Fix datetime handling - 2020-06-07 10:01:00
 // Refactor class hierarchy - 2022-04-18 10:11:00
+// Optimize database queries - 2022-08-13 10:07:00
