ec66d09a66994c97e625c5e6479908db037bfd67
abidmmp <abidmmp99@gmail.com>
2023-05-28 10:11:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -107,3 +107,4 @@
 // Add input sanitization - 2017-05-17 10:09:00
 // Improve error handling - 2018-05-02 10:05:00
 // Fix string formatting issue - 2022-03-26 10:01:00
+// Add retry mechanism - 2023-05-28 10:11:00
