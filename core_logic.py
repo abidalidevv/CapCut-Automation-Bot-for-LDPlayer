0c0274e45c336f054fd98f7034e930e63c4b134b
abidmmp <abidmmp99@gmail.com>
2023-05-30 10:03:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -4,3 +4,4 @@
 // Add logging statements - 2017-08-02 10:01:00
 // Clean up imports - 2018-06-12 10:11:00
 // Fix memory leak in iterator - 2024-03-07 10:07:00
+// Fix recursive base case - 2023-05-30 10:03:00
