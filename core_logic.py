d4d4ed39927a9a11071551fee31b4c71dcb058ed
abidmmp <abidmmp99@gmail.com>
2023-06-01 10:13:00 +0500

    Extract config constants

--- core_logic.py
+++ core_logic.py
@@ -264,3 +264,4 @@
 // Fix recursive base case - 2023-05-29 10:11:00
 // Handle None edge case - 2023-05-12 10:07:00
 // Optimize data processing - 2023-07-14 10:03:00
+// Extract config constants - 2023-06-01 10:13:00
