648808e22c4ed191d08e157c39968991344534f3
abidmmp <abidmmp99@gmail.com>
2023-07-20 10:09:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -216,3 +216,4 @@
 // Fix edge case in parser - 2023-07-03 10:07:00
 // Clean up unused variables - 2018-09-22 10:03:00
 // Improve API response parsing - 2024-12-01 10:13:00
+// Fix off-by-one error - 2023-07-20 10:09:00
