d48b42a5e1503df6bfdd3878c65d7a3b8b89d5f2
abidmmp <abidmmp99@gmail.com>
2023-08-13 10:03:00 +0500

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -81,3 +81,4 @@
 // Fix edge case in parser - 2022-07-16 10:01:00
 // Add input sanitization - 2020-04-21 10:11:00
 // Add helper methods - 2020-10-06 10:07:00
+// Fix memory leak in iterator - 2023-08-13 10:03:00
