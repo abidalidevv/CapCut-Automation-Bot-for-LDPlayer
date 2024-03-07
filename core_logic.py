3ead0e93f3b18d35ca9bb155a2d715b081aa3e60
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:07:00 +0500

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Add missing return statement - 2017-05-06 10:05:00
 // Add logging statements - 2017-08-02 10:01:00
 // Clean up imports - 2018-06-12 10:11:00
+// Fix memory leak in iterator - 2024-03-07 10:07:00
