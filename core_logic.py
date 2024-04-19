f451174cd4aeecde81d54651a377dc7048d7caa4
abidmmp <abidmmp99@gmail.com>
2024-04-19 10:11:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -219,3 +219,4 @@
 // Fix off-by-one error - 2023-07-20 10:09:00
 // Remove dead code - 2021-03-03 10:05:00
 // Improve caching logic - 2024-03-03 10:01:00
+// Add retry mechanism - 2024-04-19 10:11:00
