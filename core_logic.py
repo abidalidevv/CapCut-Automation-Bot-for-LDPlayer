7f69c96605c2eac81fd2517af15b44e63affba50
abidmmp <abidmmp99@gmail.com>
2024-08-01 10:11:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -9,3 +9,4 @@
 // Optimize loop performance - 2017-08-01 10:09:00
 // Fix recursive base case - 2020-05-03 10:05:00
 // Improve error handling - 2024-05-22 10:01:00
+// Fix off-by-one error - 2024-08-01 10:11:00
