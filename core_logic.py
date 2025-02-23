a45316eb8a7cbf7590e26047a59835c44d11f095
Abid Ali <abidmmp99@gmail.com>
2025-02-23 16:44:00 +0000

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -297,3 +297,9 @@
 
 def format_date(dt):
     return dt.strftime('%Y-%m-%d')
+
+def human_size(n):
+    for u in ['B','KB','MB','GB']:
+        if n < 1024: return f'{n:.1f} {u}'
+        n /= 1024
+    return f'{n:.1f} TB'
